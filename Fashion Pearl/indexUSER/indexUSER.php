236aae32e2778c2a253d927428f1f5724b5cdcc9
olsash <olsashala98@gmail.com>
2025-01-28 23:11:14 +0100

    me qu te njejtin kod ne user

---
+++ Fashion Pearl/indexUSER/indexUSER.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>FashionPearl || Home</title>
+  <link rel="stylesheet" href="indexADMIN.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Roboto:wght@300&display=swap">
+  <script src="https://unpkg.com/ionicons@5.5.2/dist/ionicons.js"></script>
+</head>
+<body>
+   <header class="header">
+    <a href="index.html" class="logo"><ion-icon name="menu-outline"></ion-icon></a>
+    <nav class="nav">
+      <a class="now" href="indexADMIN.php">Home</a>
+      <a href="#">Shop</a>
+      <a href="#">Categories</a>
+      <a href="#">About</a>
+      <a href="#">Support</a>
+    </nav>
+    <div class="icons">
+      <a href="#"><ion-icon name="search"></ion-icon></a>
+      <a href="#"><ion-icon name="cart"></ion-icon></a>
+      <span class="separator">|</span>
+      <a href="register.html"><ion-icon name="person-circle-outline"></ion-icon></a>
+    </div>
+  </header>
+  <main class="main-banner">
+    <div class="banner-content">
+      <h1>INDULGE <br> NOW</h1>
+      <p>Your Wardrobe, Our Planet</p>
+      <button class="shop-now-btn">Shop now</button>
+    </div>
+  </main>
+
+   <section class="photo-gallery">
+    <div class="photo">
+        <img src="../foto/model-image.png" alt="Photo 1">
+    </div>
+    <div class="photo">
+        <img src="../foto/jisoooo.jpg" alt="Photo 2">
+        <div class="photo-text">Shop Now</div>
+    </div>
+    <div class="photo">
+        <img src="../foto/foto3.jpeg" alt="Photo 3">
+        <div class="photo-text">New Collection</div>
+    </div>
+  </section>
+
+    <div class="social-links">
+    <a href="#"><ion-icon name="logo-facebook"></ion-icon></a>
+    <a href="#"><ion-icon name="logo-twitter"></ion-icon></a>
+    <a href="#"><ion-icon name="logo-linkedin"></ion-icon></a>
+  </div>
+  
+   <div class="white-ribbon"></div>
+  
+   <footer class="footer">
+  <div class="footer-section">
+    <h3>FashionPearl</h3>
+    <p>Fashion Pearl offers elegant clothing and accessories for every style and occasion, combining high quality with modern designs.</p>
+  </div>
+
+  <div class="footer-section">
+    <h3>Shop</h3>
+    <ul>
+      <li><a href="#">Men's Collection</a></li>
+      <li><a href="#">Women's Collection</a></li>
+      <li><a href="#">Accessories</a></li>
+    </ul>
+  </div>
+  <div class="footer-section">
+    <h3>Contact Us</h3>
+    <p>(+383)44 123-456</p>
+    <p><a href="mailto:support@fashionpear.com">support@fashionpearl.com</a></p>
+  </div>
+  <div class="footer-bottom">
+    <p>&copy; 2024 FashionPearl. All rights reserved.</p>
+
+  </div>
+</footer>
+</body>
+</html>
+
+
+<script src="indexADMIN.js"></script>
