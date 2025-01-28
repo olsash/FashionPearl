ddb798702527cfbd2f58cad6311a74ba0c0ca63c
olsash <olsashala98@gmail.com>
2025-01-28 23:08:46 +0100

    me lidh mire index me style

--- Fashion Pearl/index admin/indexADMIN.php
+++ Fashion Pearl/index admin/indexADMIN.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>FashionPearl || Home</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="indexADMIN.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Roboto:wght@300&display=swap">
   <script src="https://unpkg.com/ionicons@5.5.2/dist/ionicons.js"></script>
 </head>
