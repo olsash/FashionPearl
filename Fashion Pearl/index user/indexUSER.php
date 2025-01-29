32354e5f96489fd0d68037f6ea33d9fb6576d582
olsash <olsashala98@gmail.com>
2025-01-29 22:08:13 +0100

    me ndreq linkun per shop

--- Fashion Pearl/index user/indexUSER.php
+++ Fashion Pearl/index user/indexUSER.php
@@ -22,8 +22,8 @@ if (isset($_SESSION['username'])) {
    <header class="header">
    <!-- <a href="index.html" class="logo"><ion-icon name="menu-outline"></ion-icon></a> -->
     <nav class="nav">
-      <a class="now" href="indexuser.php">Home</a>
-      <a href="#">Shop</a>
+      <a class="now" href="indexUSER.php">Home</a>
+      <a href="../shop admin/shopADMIN.php">Shop</a>
       <a href="#">About</a>
       <a href="#">Support</a>
     </nav>
@@ -38,7 +38,7 @@ if (isset($_SESSION['username'])) {
     <div class="banner-content">
       <h1>INDULGE <br> NOW</h1>
       <p>Your Wardrobe, Our Planet</p>
-      <button class="shop-now-btn">Shop now</button>
+      <button class="shop-now-btn" onclick="window.location.href='../shop admin/shopADMIN.php'">Shop now</button>
     </div>
   </main>
   <div class="sidebar" id="sidebar"></div>
