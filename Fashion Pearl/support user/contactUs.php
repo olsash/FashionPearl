a597a9cc4d4f9a635a2da73f91d2907ce823e11d
fionaajeti <fionaajeti97@gmail.com>
2025-02-02 23:33:26 +0100

    input css countat form

--- Fashion Pearl/support user/contactUs.php
+++ Fashion Pearl/support user/contactUs.php
@@ -30,3 +30,46 @@
       transform: translateY(20px);
       animation: fadeInUp 1s ease-out;
     }
+
+    @keyframes fadeInUp {
+      from {
+        opacity: 0;
+        transform: translateY(40px);
+      }
+      to {
+        opacity: 1;
+        transform: translateY(0);
+      }
+    }
+  
+    label {
+      font-size: 20px;
+      font-weight: 600;
+      margin-bottom: 12px;
+      display: block;
+      color: #96a7b5;
+    }
+  
+    input[type="text"],
+    input[type="email"],
+    textarea {
+      width: 100%;
+      padding: 16px;
+      margin: 12px 0 24px 0;
+      border: 1px solid #052034;
+      border-radius: 12px;
+      font-size: 16px;
+      color: #021728;
+      background-color: #f1f3f6;
+      box-sizing: border-box;
+      transition: all 0.3s ease;
+    }
+  
+    input[type="text"]:focus,
+    input[type="email"]:focus,
+    textarea:focus {
+      border-color: #0d3044;
+      background-color: #f0f4f6;
+      box-shadow: 0 0 10px rgba(229, 234, 246, 0.3);
+      outline: none;
+    }
