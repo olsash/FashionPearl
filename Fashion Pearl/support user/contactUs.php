f4f5cd5dd64277b705e9d35dc5c06d0a2e7a0a86
fionaajeti <fionaajeti97@gmail.com>
2025-02-02 23:32:26 +0100

    contactUs forma ne php

---
+++ Fashion Pearl/support user/contactUs.php
@@ -0,0 +1,13 @@
+<form action="submit_form.php" method="POST">
+    <label for="name">Full Name:</label><br>
+    <input type="text" id="name" name="name" required><br><br>
+  
+    <label for="email">Email:</label><br>
+    <input type="email" id="email" name="email" required><br><br>
+  
+    <label for="message">Message:</label><br>
+    <textarea id="message" name="message" rows="4" required></textarea><br><br>
+  
+    <input type="submit" value="Submit">
+  </form>
+  
