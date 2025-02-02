261dc5c2f79c35328cfda2f11d233aaafaa91eea
fionaajeti <fionaajeti97@gmail.com>
2025-02-02 23:37:33 +0100

    help section ne support php

--- Fashion Pearl/support user/supportUSER.php
+++ Fashion Pearl/support user/supportUSER.php
@@ -44,3 +44,15 @@
             <div class="user-info">
                 </span>
             </div>
+
+            <div class="help-sections">
+    <?php foreach ($faqs as $faq): ?>
+    <div class="section">
+        <button class="accordion"><?= htmlspecialchars($faq['question']); ?> <span>&#9662;</span></button>
+        <div class="panel"><?= nl2br(htmlspecialchars($faq['answer'])); ?></div>
+    </div>
+    <?php endforeach; ?>
+</div>
+
+        
+    </div>
