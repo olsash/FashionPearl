a5ea71b1ad0ece6a403cfdac1a6075d4f18f2f58
fionaajeti <fionaajeti97@gmail.com>
2025-02-02 23:19:14 +0100

    login dhe sidebar per js

--- Fashion Pearl/about user/aboutUSER.js
+++ Fashion Pearl/about user/aboutUSER.js
@@ -12,3 +12,36 @@ async function fetchAboutSections() {
 }
 
 fetchAboutSections();
+
+
+function checkLogin() {
+    fetch('../profile/checkLogin.php')
+        .then(response => response.json())
+        .then(data => {
+            if (data.loggedIn) {
+                openSidebar();
+            } else {
+                window.location.href = '../login/login.php';
+            }
+        })
+        .catch(error => console.error('Gabim gjatë kontrollit të login:', error));
+  }
+  
+  
+  function openSidebar() {
+      document.getElementById('sidebar').style.display = 'block';
+      document.getElementById('overlay').style.display = 'block';
+  
+      fetch('../profile/profile.php')
+        .then(response => response.text())
+        .then(data => {
+          document.getElementById('sidebar').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading sidebar:', error));
+    }
+  
+    function closeSidebar() {
+      document.getElementById('sidebar').style.display = 'none';
+      document.getElementById('overlay').style.display = 'none';
+    }
+  
