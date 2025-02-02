59b42aec3a3a133d3f426ff11c0a21389deaa33c
olsash <olsashala98@gmail.com>
2025-02-02 19:02:32 +0100

    linkat i ndreqa

--- Fashion Pearl/shop user/productDetailsUSER.php
+++ Fashion Pearl/shop user/productDetailsUSER.php
@@ -22,7 +22,7 @@ $sizes = ['XS','S', 'M', 'L', 'XL'];
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Product Page</title>
-      <link rel="stylesheet" href="productDetailsADMIN.css">
+      <link rel="stylesheet" href="productDetailsUSER.css">
       <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Roboto:wght@300&display=swap">
       <script src="https://unpkg.com/ionicons@5.5.2/dist/ionicons.js"></script>
@@ -30,8 +30,8 @@ $sizes = ['XS','S', 'M', 'L', 'XL'];
   <div class="ribbon"></div>  
   <header class="header">
     <nav class="nav">
-      <a class="now" href="../index admin/indexADMIN.php">Home</a>
-      <a class="shop" href="shopADMIN.php">Shop</a>
+      <a class="now" href="../index user/indexUSER.php">Home</a>
+      <a class="shop" href="shopUSER.php">Shop</a>
       <a href="#">About</a>
       <a href="#">Support</a>
     </nav>
