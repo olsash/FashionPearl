1982ca15b7f332699dfcb325b6eb59b67007eae3
fionaajeti <fionaajeti97@gmail.com>
2025-02-02 23:37:11 +0100

    support user question form edhe anser container

--- Fashion Pearl/support user/supportUSER.php
+++ Fashion Pearl/support user/supportUSER.php
@@ -24,3 +24,23 @@
   <a><ion-icon name="person-circle-outline" onclick="checkLogin()"></ion-icon></a>
 </div>
 </header>
+
+<title>Help Center</title>
+    
+    </head>
+    <body>
+    <form id="questionForm">
+    <div class="qa-container">
+        <input type="text" id="question" name="question" placeholder="Ask a question..." required>
+        <button type="submit">Ask</button>
+    </div>
+    <div id="answer-container">
+        <div id="answer"></div>
+    </div>
+    </form>
+        <div class="container">
+            <h1>How can we help you?</h1>
+            <a href="contactUs.php">contact us</a>
+            <div class="user-info">
+                </span>
+            </div>
