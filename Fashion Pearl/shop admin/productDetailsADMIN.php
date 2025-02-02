85756825b46f57b35e2ad2f7835fb693ae39c678
olsash <olsashala98@gmail.com>
2025-02-02 17:30:37 +0100

    the header of the product details

---
+++ Fashion Pearl/shop admin/productDetailsADMIN.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Product Page</title>
+      <link rel="stylesheet" href="productDetailsADMIN.css">
+      <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Roboto:wght@300&display=swap">
+      <script src="https://unpkg.com/ionicons@5.5.2/dist/ionicons.js"></script>
+  </head>
+  <div class="ribbon"></div>  
+  <header class="header">
+    <nav class="nav">
+      <a class="now" href="../index admin/indexADMIN.php">Home</a>
+      <a class="shop" href="shopADMIN.php">Shop</a>
+      <a href="#">About</a>
+      <a href="#">Support</a>
+    </nav>
+    <div class="icons">
+    <a href="#" id="cart-icon">
